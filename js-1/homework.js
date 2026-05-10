110108bcc431d214847b5ea42e51f6331ca181ae
cvetano227 <cvetano227@gmail.com>
2026-05-10 17:47:10 +0200

    Having more fun with a guess my number game and a better version of password checker

--- js-1/homework.js
+++ js-1/homework.js
@@ -21,10 +21,16 @@
 //   number = number + 2;
 // }
 
-let result = 1;
-let counter = 0;
-while (counter < 10) {
-  result = result * 2;
-  counter = counter + 1;
-}
-console.log(result);
+// let result = 1;
+// let counter = 0;
+// while (counter < 10) {
+//   result = result * 2;
+//   counter = counter + 1;
+// }
+// console.log(result);
+
+let yourName;
+do {
+  yourName = prompt("Vnesete go vaseto ime:");
+} while (!yourName);
+console.log(`Zdravo, ${yourName}!`);
