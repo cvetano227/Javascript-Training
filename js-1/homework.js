b77ac7fc7ec806d423cb0da4df897b979c14b74c
cvetano227 <cvetano227@gmail.com>
2026-05-10 17:27:34 +0200

    Learning some more javascript, control flow, conditional executement, loops, etc

--- js-1/homework.js
+++ js-1/homework.js
@@ -6,3 +6,25 @@
 
 ///ALSO READ ABOUT FUNCTIONS AT
 // https://www.w3schools.com/js/js_functions.asp
+
+// const roundTo = function (n, step) {
+//   let remainder = n % step;
+//   return n - remainder + (remainder < step / 2 ? 0 : step);
+// };
+
+// console.log(roundTo(7, 5)); // Output: 5
+// console.log(roundTo(12, 5));
+
+// let number = 0;
+// while (number <= 100) {
+//   console.log(number);
+//   number = number + 2;
+// }
+
+let result = 1;
+let counter = 0;
+while (counter < 10) {
+  result = result * 2;
+  counter = counter + 1;
+}
+console.log(result);
