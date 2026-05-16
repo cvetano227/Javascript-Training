27c9af5b781143c3e84b8def2c5c6d700dde57f6
cvetano227 <cvetano227@gmail.com>
2026-05-16 22:09:12 +0200

    Created a Navbar hamburger menu

--- DOM Training/script.js
+++ DOM Training/script.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const icon = document.querySelector(".icon");
+const links = document.querySelector("#headerLinks");
+
+icon.addEventListener("click", function () {
+  links.classList.toggle("active");
+});
